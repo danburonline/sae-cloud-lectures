3da0481ffddc9c23fd08dc0103c51f30c15b7393
Daniel Burger <public@danielburger.online>
2024-01-22 16:32:47 +0100

    Create Dockerfile

--- 01_dockerizing/src/app.controller.ts
+++ 01_dockerizing/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRandomData(): { timestamp: string; randomWords: string } {
+    return this.appService.getRandomData();
   }
 }
--- 01_dockerizing/src/app.service.ts
+++ 01_dockerizing/src/app.service.ts
@@ -2,7 +2,38 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private randomWords = [
+    'apple',
+    'banana',
+    'cherry',
+    'date',
+    'elderberry',
+    'fig',
+    'grape',
+    'honeydew',
+    'jackfruit',
+    'kiwi',
+    'lemon',
+  ];
+
+  getRandomData(): { timestamp: string; randomWords: string } {
+    const result = [];
+    let lastWord = '';
+
+    for (let i = 0; i < 4; i++) {
+      let word = '';
+      do {
+        word =
+          this.randomWords[Math.floor(Math.random() * this.randomWords.length)];
+      } while (word === lastWord);
+
+      result.push(word);
+      lastWord = word;
+    }
+
+    return {
+      timestamp: new Date().toISOString(),
+      randomWords: result.join(' '),
+    };
   }
 }
